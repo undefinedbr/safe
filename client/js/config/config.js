e65058e265db06ab37b43a772d9bf7b43b4bb072
Lucas Henrique de Abreu <lucas.henrique@viasoft.com.br>
2017-01-18 11:08:08 -0200

    adicionado arquivos de configuração

---
+++ client/js/config/config.js
@@ -0,0 +1,43 @@
+/**
+ * @autor -  Lucas Henrique de Abreu - <lucasigual14@gmail.com>
+ * Modulo : Módulo de configurações.
+ * data: 18/01/2017
+ */
+(function(angular) {
+	'use strict';
+	angular.module('app.routes', ['ngRoute', 'ngMaterial'])
+
+	.config(function ($mdDateLocaleProvider, $mdThemingProvider, $mdAriaProvider) {
+		$mdDateLocaleProvider.formatDate = function (date) {
+			return date ? moment(date).format('DD/MM/YYYY') : '';
+		};
+
+		$mdDateLocaleProvider.parseDate = function (dateString) {
+			var m = moment(dateString, 'DD/MM/YYYY', true);
+			return m.isValid() ? m.toDate() : new Date(NaN);
+		};
+		$mdThemingProvider.theme('default').primaryPalette('teal');
+		$mdAriaProvider.disableWarnings();
+	})
+	.config(['$routeProvider', function (routeProvider) {
+		routeProvider.when('/home', {
+			templateUrl: 'partials/home.html',
+			controller: 'HomeController',
+			controllerAs: 'ctrl'
+		}).otherwise({
+			redirectTo: '/crud'
+		});
+	}])
+	.run(function ($rootScope, $location) {
+		var history = [];
+
+		$rootScope.$on('$routeChangeSuccess', function () {
+			history.push($location.$$path);
+		});
+
+		$rootScope.back = function () {
+			var prevUrl = history.length > 1 ? history.splice(-2)[0] : "/";
+			$location.path(prevUrl);
+		};
+	});
+})(window.angular);
---
+++ client/server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.use(express.static(path.join(__dirname, '/')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000);
+
+console.log("Running at Port 3000");
